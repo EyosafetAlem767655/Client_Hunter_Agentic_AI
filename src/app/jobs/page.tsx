62838cbabb37a78d97ccb0d6095ad621e9010104
EyosafetAlem767655 <eyosafetalem@gmail.com>
2026-07-07 13:28:29 +0300

    Great fix number 1 relevancy tab bug fix 2

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -182,6 +182,7 @@ export default async function JobsPage({
 
       {!error && activeTabKey === "jobs" && (
         <JobsView
+          key={`${activeSort}-${activeWindow}-${currentPage}`}
           jobs={jobs}
           total={total}
           currentPage={currentPage}
